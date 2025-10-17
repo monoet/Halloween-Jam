7161357d32ae3725d6ff74245ce2dc3f4822fcc0
monoet <50036734+monoet@users.noreply.github.com>
2025-10-17 01:34:57 -0600

    Move legacy combat pipeline into Legacy/CombatV1

--- Assets/Scripts/BattleV2/Providers/IBattleInputProvider.cs
+++ Assets/Scripts/BattleV2/Providers/IBattleInputProvider.cs
@@ -1,9 +1,10 @@
 using System;
+using BattleV2.Actions;
 
 namespace BattleV2.Providers
 {
     public interface IBattleInputProvider
     {
-        void RequestAction(BattleActionContext context, Action<Actions.ActionData> onSelected, Action onCancel);
+        void RequestAction(BattleActionContext context, Action<ActionData> onSelected, Action onCancel);
     }
 }
